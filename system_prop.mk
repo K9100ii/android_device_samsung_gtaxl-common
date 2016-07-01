917689d31d078f9a8b0e2d2595f0c41d25dee720
rago <rago@google.com>
2016-07-01 16:05:00 -0700

    gtaxl-common: Set voice call volume steps to 7
    Bug: 28426597
    Change-Id: Icf78d9acec75c500982b1e4391af4d6f3933da37

--- system_prop.mk
+++ system_prop.mk
@@ -2,7 +2,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     ro.vendor.audio_hal.force_voice_config=wide \
-    ro.config.media_vol_steps=25
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
