7269eaabdc8855c4ba6c459aa7454cbdfb70883e
Kyle Harrison <khwebmail@gmail.com>
2021-10-14 10:00:52 +0100

    gtaxl-common: Use threaded GLES render engine
    Change-Id: I3ac8f4f37b88ccd95cf651b607705651d9506a20

--- system_prop.mk
+++ system_prop.mk
@@ -36,7 +36,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
-    debug.sf.disable_backpressure=1
+    debug.sf.disable_backpressure=1 \
+    debug.renderengine.backend=threaded
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
