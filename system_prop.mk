ef583e496d109a052b0cba7e3a60638a7a731065
TALU <taluemail272@gmail.com>
2022-10-14 17:15:53 +0100

    gtaxl-common: media: Import configs from M105FDDS4CVG1 (with some changes)
    Change-Id: I3153a80b807e87ee736aea4d5e5750087651c7f6

--- system_prop.mk
+++ system_prop.mk
@@ -47,10 +47,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     lockscreen.rot_override=true
 
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.ccodec=0
-
 # sdcardfs
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
