7713e17816f51326bd44af99277d59fd452301b6
LuK1337 <priv.luk@gmail.com>
2022-11-03 20:57:11 +0100

    gtaxl-common: Add hardware/samsung to soong namespaces
    See I78b1b9930a57bc01afda722b9f8a196c372f9d8b.
    
    Change-Id: I634b562e12b70a0c060d449d3ba1d4e87204cf5d

--- common.mk
+++ common.mk
@@ -235,7 +235,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/samsung
 
 # Touch features
 PRODUCT_PACKAGES += \
