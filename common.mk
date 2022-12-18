b99273615afa14c69f22ff6a2a56acaaea9477e0
TALU <taluemail272@gmail.com>
2022-12-18 17:14:05 +0000

    gtaxl-common: Upgrade android.hardware.keymaster to 4.1
    Change-Id: I55ded8ab6d4efce9a831ec885d44eec7e892255c

--- common.mk
+++ common.mk
@@ -111,8 +111,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.0-service
+    android.hardware.keymaster@4.1-service
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
