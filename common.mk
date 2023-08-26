56d31ebdf7d045a148df39f4681ee64a10ac786f
K9100ii <k9100ii.ki@gmail.com>
2023-08-26 12:46:39 +0100

    gtaxl-common: Revert "Switch BtAudio to AIDL"
    This reverts commit f1128e4b53f65b1936c9b2e11d464f8e6932652e.
    
    Backed into using it, AIDL just isn't working.
    
    Change-Id: Ib376d63896328b21f7329e8d6a77218d72b75e6e

--- common.mk
+++ common.mk
@@ -25,7 +25,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service.gtaxl \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     audio.primary.exynos7870_32 \
     audio.r_submix.default \
